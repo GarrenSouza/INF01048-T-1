2eb80646d825033017b2d10b69f99f57e45eedf3
GarrenSouza <contato.garren@gmail.com>
2021-08-29 18:10:12 -0300

    astar_manhattan and astar_hamming were added

--- kit_busca/solucao.py
+++ kit_busca/solucao.py
@@ -1,4 +1,7 @@
-OBJETIVO = "12345678_"
+import heapq as hpq
+import math
+
+_OBJETIVO = "12345678_"
 
 
 def swap(estado, i1, i2):
@@ -7,6 +10,26 @@ def swap(estado, i1, i2):
     return ''.join(estado)
 
 class Nodo:
+    # __lt__, __gt__, __le__, __ge__, __eq__, and __ne__
+
+    def __lt__(self, nodo):
+        return self.get_custo() < nodo.get_custo()
+
+    def __le__(self, nodo):
+        return self.get_custo() <= nodo.get_custo()
+
+    def __gt__(self, nodo):
+        return self.get_custo() > nodo.get_custo()
+
+    def __ge__(self, nodo):
+        return self.get_custo() >= nodo.get_custo()
+
+    def __eq__(self, nodo):
+        return self.get_custo() == nodo.get_custo()
+
+    def __ne__(self, nodo):
+        return self.get_custo() != nodo.get_custo()
+
     """
     Implemente a classe Nodo com os atributos descritos na funcao init
     """
@@ -23,24 +46,24 @@ class Nodo:
         :param custo:int, custo do caminho da raiz até este nó
         """
 
-    def getCusto(self):
+    def get_custo(self):
         return self.custo
     
-    def getEstado(self):
+    def get_estado(self):
         return self.estado
 
-    def getPai(self):
+    def get_pai(self):
         return self.pai
 
-    def getAcao(self):
+    def get_acao(self):
         return self.acao
 
     def caminho(self):
         caminho = []
-        pai = self.getPai()
+        pai = self.get_pai()
         while pai:
-            caminho.insert(0, pai.getAcao())
-            pai = pai.getPai()
+            caminho.insert(0, pai.get_acao())
+            pai = pai.get_pai()
         return caminho
 
     # def caminho(self):
@@ -91,9 +114,9 @@ def expande(nodo):
     :return:
     """
 
-    return [Nodo(estado, nodo, acao, nodo.getCusto() + 1) for acao, estado  in sucessor(nodo.estado)]
+    return [Nodo(estado, nodo, acao, nodo.get_custo() + 1) for acao, estado  in sucessor(nodo.estado)]
 
-def retiraBFS(F):
+def retira_BFS(F):
     return (F[0], F[1:])
 
 def bfs(estado):
@@ -110,14 +133,21 @@ def bfs(estado):
     F = [Nodo(estado, None, None, 0)]
 
     while F:
-        v, F = retiraBFS(F)
-        if v.getEstado() == OBJETIVO:
+        [print(e.get_estado()) for e in X.values()]
+        input()
+        v, F = retira_BFS(F)
+        if v.get_estado() == _OBJETIVO:
             return v.caminho()
-        if v.getEstado() not in X:
-            X[v.getEstado()] = v
+        # print(v.get_estado())
+        # print(X)
+        # input()
+        if v.get_estado() not in X:
+            X[v.get_estado()] = v
             F = F + expande(v)
     return None
 
+def retira_DFS(F):
+    return (F[-1], F[:-1])
 
 def dfs(estado):
     """
@@ -128,9 +158,31 @@ def dfs(estado):
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
 
+    X = {}
+    F = [Nodo(estado, None, None, 0)]
+
+    while F:
+        [print(e.get_estado()) for e in X.values()]
+        input()
+        v, F = retira_DFS(F)
+        if v.get_estado() == _OBJETIVO:
+            return v.caminho()
+        if v.get_estado() not in X:
+            X[v.get_estado()] = v
+            F = F + expande(v)
+    return None
+
+def retira_astar(estado):
+    return hpq.heappop(estado)
+
+def get_hamming_distance(estado):
+    return len([1 for i, e in enumerate(estado) if '12345678_'.find(e) != i])
+
+def expande_astar_hamming(F, nodo):
+    l = F + [Nodo(estado, nodo, acao, nodo.get_custo() + 1 + get_hamming_distance(estado)) for acao, estado  in sucessor(nodo.estado)]
+    hpq.heapify(l)
+    return l
 
 def astar_hamming(estado):
     """
@@ -141,10 +193,44 @@ def astar_hamming(estado):
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    """
+    Recebe um estado (string), executa a busca em PROFUNDIDADE e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
 
+    X = {}
+    F = [Nodo(estado, None, None, 0)]
 
+    while F:
+        v = retira_astar(F)
+        if v.get_estado() == _OBJETIVO:
+            return v.caminho()
+        if v.get_estado() not in X:
+            X[v.get_estado()] = v
+            F = expande_astar_hamming(F, v)
+
+    return None
+
+def get_manhattan_distance(estado):
+  distancia = 0.
+  for i, e in enumerate(estado):
+      x = i%3
+      y = int(i/3)
+      i_real = _OBJETIVO.find(e)
+      x_real = i_real%3
+      y_real = int(i_real/3)
+      distancia = distancia + math.sqrt((x - x_real)**2 + (y - y_real)**2)
+  return distancia
+
+def expande_astar_manhattan(F, nodo):
+    l = F + [Nodo(estado, nodo, acao, nodo.get_custo() + 1 + get_manhattan_distance(estado)) for acao, estado  in sucessor(nodo.estado)]
+    hpq.heapify(l)
+    return l
+    
 def astar_manhattan(estado):
     """
     Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Manhattan e
@@ -154,7 +240,18 @@ def astar_manhattan(estado):
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
 
-len(bfs("2_3541687")) == 23
+    X = {}
+    F = [Nodo(estado, None, None, 0)]
+
+    while F:
+        v = retira_astar(F)
+        if v.get_estado() == _OBJETIVO:
+            return v.caminho()
+        if v.get_estado() not in X:
+            X[v.get_estado()] = v
+            F = expande_astar_manhattan(F, v)
+
+    return None
+
+print(len(astar_manhattan("2_3541687")) == 23)
